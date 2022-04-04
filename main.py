340d9c32f05113a078decf39d90edb9607564681
HuyTrinh <trnhquchuy@yahoo.com.vn>
2022-04-05 01:55:18 +0700

    update full

--- main.py
+++ main.py
@@ -1,19 +1,23 @@
 from utils import *
 from LinearRegression import LinearRegression
 
+# from sklearn.metrics import mean_absolute_error
 linear = LinearRegression(graphfile)
 X,y = create_data(csv_file,X_name,y_name)
 print(X.shape)
 
-x_train,y_train,x_test,y_test = train_test_split(X,y)
+x_train,y_train,x_test,y_test = train_test_split(X,y,0.2)
 print("Starting update weight ...")
 linear.fit(X,y)
 
+print("plot data distribution in 3D")
+plot_3d_graph(X,y)
+
+print("plot data distribution in 2D")
+plot_2d_graph(X,y)
+
 print("Weight after update:")
 linear.get_weight()
-
-# print("Graph for the linear:")
-# # linear.plot_graph()
 
 sample = readfile(inputfile)
 print(sample)
@@ -26,12 +30,21 @@ for i in range(len(sample)):
 writefile(outfile,out)
 
 y_predict = linear.predict(x_test)
-print(y_predict)
+
+print("comparing with the true label")
+compare(y_test,y_predict)
+
+print("Graph for the linear on test set")
+plot_line_2d(y_test,y_predict)
+
 print("Testing model:")
+
 mae = linear.mae(y_predict,y_test)
 mse = linear.mse(y_predict,y_test)
 print("Mae: ",mae)
 print("MSE: ",mse)
+
 f = open(evalfile,"w")
 f.write("Mae score: " + str(mae) +"\n"
 		+"Mse score: " + str(mse)+"\n")
+
--- utils.py
+++ utils.py
@@ -2,13 +2,18 @@ import numpy as np
 import pandas as pd
 import math
 import matplotlib.pyplot as plt
-X_name = ["MLY-TMIN-NORMAL","MLY-TMIN-NORMAL"]
-y_name = ["MLY-PRCP-NORMAL"]
-csv_file = "./asset/data.csv"
+import seaborn as sns
+
+X_name = ["HLY-TEMP-NORMAL","HLY-PRES-NORMAL"]
+y_name = ["HLY-DEWP-NORMAL"]
+csv_file = "NORMAL_HLY_sample_csv.csv"
 inputfile ="./asset/test.txt"
 outfile = "./asset/out.txt"
 graphfile = "./asset/graph_out.png"
 evalfile = "./asset/evaluation.txt"
+graph2d = "./asset/distribution_2d.png"
+graph3d = "./asset/distribution_3d.png"
+comparegraph = "./asset/comparegraph.png"
 def create_data(csv_file,X_name,y_name):
 	X = []
 	y = []
@@ -55,3 +60,35 @@ def train_test_split(X,y, ratio = 0.2):
 		test_x.append(X[i])
 		test_y.append(y[i])
 	return np.array(train_x), np.array(train_y),np.array(test_x),np.array(test_y)
+
+def plot_2d_graph(X,y):
+	x1 = []
+	x2 = []
+	for item in X:
+		x1.append(item[0])
+		x2.append(item[1])
+	fig, ax = plt.subplots()
+	scat = ax.scatter(x1,x2, c = y, s=200)
+	fig.colorbar(scat)
+	plt.savefig(graph2d)
+
+def plot_3d_graph(X,y):
+	x1 = []
+	x2 = []
+	for item in X:
+		x1.append(item[0])
+		x2.append(item[1])
+	fig = plt.figure()
+	ax = fig.add_subplot(111, projection='3d')
+	ax.scatter(x1, x2, y)
+	plt.savefig(graph3d)
+
+def plot_line_2d(y_test,y_pred):
+	graph  = sns.regplot(x=y_test, y=y_pred, ci=None, color="b")
+	graph.get_figure()
+	plt.savefig(graphfile)
+
+def compare(y_true,y_pred):
+	ax1 = sns.distplot(y_true, hist=False, color="r", label="Actual Value")
+	sns.distplot(y_pred, hist=False, color="b", label="Fitted Values" , ax=ax1)
+	plt.savefig(comparegraph)
